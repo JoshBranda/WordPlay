44e4f3cf3bb6a64a982a114e04fd32f870a3184a
LaurynhaDO <106277308+LaurynhaDO@users.noreply.github.com>
2022-09-05 09:31:01 -0300

    Revert "Laura's updates"

--- app/src/main/java/com/example/wordplay/MainActivity.java
+++ app/src/main/java/com/example/wordplay/MainActivity.java
@@ -2,29 +2,13 @@ package com.example.wordplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.style.BackgroundColorSpan;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-    private Switch btnDarkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnDarkMode = findViewById(R.id.SwitchDarkmode);
-        btnDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                       set.BackgroundColor(Color.BLACK);
-                }
-            }
-        }
-
     }
 }
